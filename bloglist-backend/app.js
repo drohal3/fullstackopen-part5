77f6bd989f2bffd8f9a25dc030d5f9ed788168ab
Dominik Rohal <rohal.dominik.123@gmail.com>
2022-10-11 12:00:20 +0300

    exercise 5.1 init

---
+++ bloglist-backend/app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+require('dotenv').config()
+// const {info, error} = require('./utils/logger') // alternative - info() and error()
+                                                  // instead of logger.info() and logger.error()
+const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+const middleware = require('./utils/middleware')
+
+app.use(cors())
+app.use(express.json())
+app.use(middleware.tokenExtractor)
+app.use(middleware.requestLogger)
+app.use('/api/blogs', middleware.userExtractor, blogsRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
